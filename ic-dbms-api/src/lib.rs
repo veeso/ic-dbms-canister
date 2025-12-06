ddb8d8ddabaa1300f115c942d091f4e58bc823a4
veeso <christian.visintin@veeso.dev>
2025-12-06 14:13:13 +0100

    docs: Api index

--- ic-dbms-api/src/lib.rs
+++ ic-dbms-api/src/lib.rs
@@ -12,6 +12,82 @@
 //! ```rust
 //! use ic_dbms_api::prelude::*;
 //! ```
+//!
+//! # Types
+//!
+//! ## DBMS
+//!
+//! ### Database
+//!
+//! - [`Database`](crate::prelude::Database)
+//!
+//! ### Foreign Fetcher
+//!
+//! - [`ForeignFetcher`](crate::prelude::ForeignFetcher)
+//!
+//! ### Query
+//!
+//! - [`DeleteBehavior`](crate::prelude::DeleteBehavior)
+//! - [`Filter`](crate::prelude::Filter)
+//! - [`Query`](crate::prelude::Query)
+//! - [`QueryBuilder`](crate::prelude::QueryBuilder)
+//! - [`QueryError`](crate::prelude::QueryError)
+//! - [`QueryResult`](crate::prelude::QueryResult)
+//! - [`OrderDirection`](crate::prelude::OrderDirection)
+//! - [`Select`](crate::prelude::Select)
+//!
+//! ### Table
+//!
+//! - [`ColumnDef`](crate::prelude::ColumnDef)
+//! - [`ForeignKeyDef`](crate::prelude::ForeignKeyDef)
+//! - [`InsertRecord`](crate::prelude::InsertRecord)
+//! - [`TableColumns`](crate::prelude::TableColumns)
+//! - [`TableError`](crate::prelude::TableError)
+//! - [`TableName`](crate::prelude::TableName)
+//! - [`TableRecord`](crate::prelude::TableRecord)
+//! - [`UpdateRecord`](crate::prelude::UpdateRecord)
+//! - [`ValuesSource`](crate::prelude::ValuesSource)
+//!
+//! ### Transaction
+//!
+//! - [`TransactionError`](crate::prelude::TransactionError)
+//! - [`TransactionId`](crate::prelude::TransactionId)
+//!
+//! ### Types
+//!
+//! - [`Blob`](crate::prelude::Blob)
+//! - [`Boolean`](crate::prelude::Boolean)
+//! - [`Date`](crate::prelude::Date)
+//! - [`DateTime`](crate::prelude::DateTime)
+//! - [`Decimal`](crate::prelude::Decimal)
+//! - [`Int32`](crate::prelude::Int32)
+//! - [`Int64`](crate::prelude::Int64)
+//! - [`Nullable`](crate::prelude::Nullable)
+//! - [`Principal`](crate::prelude::Principal)
+//! - [`Text`](crate::prelude::Text)
+//! - [`Uint32`](crate::prelude::Uint32)
+//! - [`Uint64`](crate::prelude::Uint64)
+//! - [`Uuid`](crate::prelude::Uuid)
+//!
+//! ### Value
+//!
+//! - [`Value`](crate::prelude::Value)
+//!
+//! ## Memory
+//!
+//! - [`DataSize`](crate::memory::DataSize)
+//! - [`Encode`](crate::memory::Encode)
+//! - [`DecodeError`](crate::memory::DecodeError)
+//! - [`MemoryError`](crate::memory::MemoryError)
+//! - [`MemoryResult`](crate::memory::MemoryResult)
+//! - [`MSize`](crate::memory::MSize)
+//! - [`Page`](crate::memory::Page)
+//! - [`PageOffset`](crate::memory::PageOffset)
+//!
+//! ## Interact with an IC DBMS Canister
+//!
+//! TODO:
+//!
 
 #![doc(html_playground_url = "https://play.rust-lang.org")]
 #![doc(
